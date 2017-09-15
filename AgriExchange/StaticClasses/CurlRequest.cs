edeccfb98c7923dce7ce5c85e873dff0a13d8eac
camschoch <camschoch3@gmail.com>
2017-09-15 14:39:50 -0500

    added static classes folder

---
+++ AgriExchange/StaticClasses/CurlRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgriExchange.StaticClasses
+{
+    static public class CurlRequest
+    {
+        public static void thing()
+        {
+            
+        }
+        //possibly make the curl request return an http request may get rid of this class
+    }
+}
