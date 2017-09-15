1ea5f9dbf0dada6641132ac77c51e3ed7c409ca2
camschoch <camschoch3@gmail.com>
2017-09-15 13:27:32 -0500

    first commit

---
+++ AgriExchange/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgriExchange.Startup))]
+namespace AgriExchange
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
