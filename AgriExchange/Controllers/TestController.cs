32500dfcde164aaba0f22a77a34f16151c866012
camschoch <camschoch3@gmail.com>
2017-09-17 17:02:50 -0500

    testing get curl

---
+++ AgriExchange/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using AgriExchange.StaticClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgriExchange.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            CurlRequest.thing();
+            return View();
+        }
+    }
+}
--- AgriExchange/StaticClasses/CurlRequest.cs
+++ AgriExchange/StaticClasses/CurlRequest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AgriExchange.Models.SkyWise;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +11,18 @@ namespace AgriExchange.StaticClasses
     {
         public static void thing()
         {
-            
+            var client = new RestClient("https://insight.api.wdtinc.com/daily-high-temperature/35.191/-97.439");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "586e3421-1f96-a0fd-f4d7-5af711fba4e3");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("authorization", "Basic NjhmODM5MGE6ZDBmNTc2ZjM3MjYxZTk4NWUzZmE4YTk3YTZlMDUyNTg=");
+            IRestResponse<WeatherData> response = client.Execute<WeatherData>(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(response.Data.startDate);
+                Console.WriteLine(response.Data.endDate);
+            }
         }
         //possibly make the curl request return an http request may get rid of this class
     }
