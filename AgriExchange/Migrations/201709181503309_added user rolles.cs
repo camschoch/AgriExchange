7dbd33d9d53ec4b353fb54b6a6e0748c4f77e05d
lars-alfonse <lars.alfonse@gmail.com>
2017-09-18 10:07:15 -0500

    added roles to db

---
+++ AgriExchange/Migrations/201709181503309_added user rolles.cs
@@ -0,0 +1,16 @@
+namespace AgriExchange.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addeduserrolles : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- AgriExchange/Models/AccountViewModels.cs
+++ AgriExchange/Models/AccountViewModels.cs
@@ -79,6 +79,7 @@ namespace AgriExchange.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        public string UserRoles { get; set; }
     }
 
     public class ResetPasswordViewModel
