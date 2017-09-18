2e54bb9be6e444b8d7b0dd31ed17ab38fdce59df
lars-alfonse <lars.alfonse@gmail.com>
2017-09-18 11:26:30 -0500

    added dbcontext to home controller, added vendor form action

--- AgriExchange/Controllers/HomeController.cs
+++ AgriExchange/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AgriExchange.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,28 @@ namespace AgriExchange.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext context;
+        public HomeController()
+        {
+            context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult SubmitVendorApplication()
+        {
+            VendorApplications model = new VendorApplications();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult SubmitVendorApplication(VendorApplications model)
+        {
+
+            return RedirectToAction("SubmissionSuccess");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
