ac0dd088d581e7dc4b95d63f8228870d21cf00a7
lars-alfonse <lars.alfonse@gmail.com>
2017-09-18 11:21:04 -0500

    added controllers for admin gardener and vender

---
+++ AgriExchange/Controllers/GardenerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgriExchange.Controllers
+{
+    public class GardenerController : Controller
+    {
+        // GET: Gardener
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
