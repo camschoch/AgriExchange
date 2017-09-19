c136b380648b43eed65914e72033c47d83123073
lars-alfonse <lars.alfonse@gmail.com>
2017-09-19 13:13:05 -0500

    commit pull

---
+++ AgriExchange/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using AgriExchange.StaticClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgriExchange.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CurlCall()
+        {
+            string hourlyTemp = "daily-high-temperature";
+            string location = "/43.029494/-87.904047";
+            CurlRequest.Curl(hourlyTemp, location);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult HttpCall()
+        {
+            string searchName = "apple";
+            string typeSearch = "search=";
+            CurlRequest.Http(searchName, typeSearch);
+            return View();
+        }
+    }
+}
--- AgriExchange/Controllers/VendorController.cs
+++ AgriExchange/Controllers/VendorController.cs
@@ -3,38 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AgriExchange.Models;
 
 namespace AgriExchange.Controllers
 {
     public class VendorController : Controller
     {
-        ApplicationDbContext context;
-        public VendorController()
-        {
-            context = new ApplicationDbContext();
-            
-        }
         // GET: Vendor
         public ActionResult Index()
         {
             return View();
-        }
-        public ActionResult SubmitApplication()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult SubmitApplication(VendorApplications model)
-        {
-            model.IsApproved = false;
-            context.VendorApplications.Add(model);
-            context.SaveChanges();
-            return RedirectToAction("Index", "Home");
-        }
-
-
-
+        } 
     }
-
 }
