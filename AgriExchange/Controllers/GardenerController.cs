92a26e744634fc12e9a33282200105e3a8a44e2e
lars-alfonse <lars.alfonse@gmail.com>
2017-09-19 14:06:23 -0500

    added garden index view model

--- AgriExchange/Controllers/GardenerController.cs
+++ AgriExchange/Controllers/GardenerController.cs
@@ -11,6 +11,7 @@ namespace AgriExchange.Controllers
         // GET: Gardener
         public ActionResult Index()
         {
+
             return View();
         }
     }
---
+++ AgriExchange/Models/ViewModels/GardenerIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgriExchange.Models.ViewModels
+{
+    public class GardenerIndexViewModel
+    {
+        List<Forcast> Forcast { get; set; }
+        public GardenerIndexViewModel()
+        {
+            Forcast = new List<Forcast>();
+        }
+    }
+}
