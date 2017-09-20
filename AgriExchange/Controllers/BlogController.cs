3c3033f5edaaf7634aa35b18805a47cf6b927d0a
lars-alfonse <lars.alfonse@gmail.com>
2017-09-20 07:49:08 -0500

    added action for creating blog post

--- AgriExchange/Controllers/BlogController.cs
+++ AgriExchange/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AgriExchange.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace AgriExchange.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
+        ApplicationDbContext context;
+        public BlogController()
+        {
+            context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create(BlogPost model)
+        {
+            model.User = StaticClasses.UserRetriever.RetrieveUser(User);
+            model.DatePosted = DateTime.Now;
+            context.BlogPosts.Add(model);
+            context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
