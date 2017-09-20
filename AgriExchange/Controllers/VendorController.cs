c9ab05149e1fe9a4e81fe256254c846afd245157
tgood111 <tgood111@live.com>
2017-09-20 10:51:51 -0500

    added Admin role requirement to viewvendor

--- AgriExchange/Controllers/VendorController.cs
+++ AgriExchange/Controllers/VendorController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 using AgriExchange.Models.ViewModels;
 
@@ -37,13 +38,23 @@ namespace AgriExchange.Controllers
 
         public ActionResult ViewVendor()
         {
-            string roleId = (from data in context.Roles where data.Name == "Vendor" select data.Id).First();
-           AdminVendorViewModel model = new AdminVendorViewModel();
-           
-           model.Vendors = context.users
-           model.Applications= context.VendorApplications.ToList;
+            AdminVendorViewModel model;
+            if (User.IsInRole("Adminvendor"))
+            {
+                string roleId = (from data in context.Roles where data.Name == "Vendor" select data.Id).First();
+                model = new AdminVendorViewModel();
 
-            return View();
+                model.Vendors = context.Users.Where(x => x.Roles.Select(y => y.RoleId).Contains(roleId)).ToList();
+                model.Applications = context.VendorApplications.ToList();
+            }
+            else
+            {
+                
+                 model = new AdminVendorViewModel();
+            }
+           
+
+            return View(model);
 
 
         }
