2007719f710a2ae15b3b4e61b5fcd52dc9a1b7be
camschoch <camschoch3@gmail.com>
2017-09-25 08:54:51 -0500

    pushing to add tabs

--- AgriExchange/Controllers/GardenerController.cs
+++ AgriExchange/Controllers/GardenerController.cs
@@ -20,11 +20,14 @@ namespace AgriExchange.Controllers
         public ActionResult Index()
         {
             var user = UserRetriever.RetrieveUser(User, context);
-            var zone = (from data in context.UserAddresses.Include("Address") where data.User.Id == user.Id select data.Address.Zone).First();
-            var refinedZone = int.Parse(zone.Replace("a", String.Empty).Replace("b", String.Empty));
+            var zone = (from data in context.UserAddresses.Include("Address") where data.User.Id == user.Id select data.Address.Zone).ToList();
             GardenerIndexViewModel model = new GardenerIndexViewModel();
-            model.Reccomentdations = (from data in context.PlantZones.Include("Plant") where data.Zone.ID == refinedZone select data.Plant).ToList();
-            model.Forcast = ForcastRetriever.GetForcast(User);
+            if (zone.Count > 0)
+            {
+                var refinedZone = int.Parse(zone[0].Replace("a", String.Empty).Replace("b", String.Empty));
+                model.Reccomentdations = (from data in context.PlantZones.Include("Plant") where data.Zone.ID == refinedZone select data.Plant).ToList();
+                model.Forcast = ForcastRetriever.GetForcast(User);
+            }
             model.Blogs = (from data in context.BlogPosts where data.User.Id == user.Id select data).ToList();
             model.CropEntries = (from data in context.CropEntries where data.User.Id == user.Id select data).ToList();
             model.Follows = (from data in context.Follows.Include("FollowedUser") where data.User.Id == user.Id select data).ToList();
